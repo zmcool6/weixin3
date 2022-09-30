5729a1b3db78d792268541da9329c4cc8be43cf8
zmcool6 <49740643+zmcool6@users.noreply.github.com>
2022-09-30 16:02:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_weather(region):
     # 天气
     weather = response["now"]["text"]
     # 当前温度
-    temp = response["now"]["temp"] + u"\N{DEGREE SIGN}" + "C"
+    temp = response["now"]["temp"]
     # 风向
     wind_dir = response["now"]["windDir"]
     return weather, temp, wind_dir
@@ -146,7 +146,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
                 "color": get_color()
             },
             "region": {
-                "value": region_name,
+                "value": "贵阳市",
                 "color": get_color()
             },
             "weather": {
